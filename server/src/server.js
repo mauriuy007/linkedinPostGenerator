f1ca18f07eb4078756bd3c6238dacfff27f5b4af
mauriuy007 <mauricio.parodivar@gmail.com>
2026-03-18 23:19:24 -0300

    login & register endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,10 @@
 import express from 'express'; 
+import authRoutes from './routes/auth.routes.js';
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-app.post('/', (req, res) => {
-    console.log('Testing') 
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
